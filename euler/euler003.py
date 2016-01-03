962e002991e1503259d323243cba334152d7c0e5
thomastien <thomas.tien@gmail.com>
2016-01-03 01:26:29 -0500

    Largest Prime Factor (Solved)
    Sample input:
    
    6
    111
    3898
    11696
    11694
    24
    17

--- euler/euler003.py
+++ euler/euler003.py
@@ -9,9 +9,9 @@ samples = input()
 # primes: dictionary of prime numbers
 # inputs: list of parameters
 primes = {}
-#lpfs = {}
 lpfs = defaultdict(list)
 inputs = []
+debug = 0
 
 #pp = pprint.PrettyPrinter;
     
@@ -20,6 +20,11 @@ def isFactor(n,d):
         return True
     else:
         return False
+    
+def reduce(orig,factor):
+    while (orig%factor ==0):
+        orig = orig/factor
+    return orig
 
     
 for i in range(0,samples):
@@ -27,13 +32,15 @@ for i in range(0,samples):
     #inputs.insert(0,targetval)
     inputs.append(targetval)
 
-print max(inputs) 
+if debug:
+    print max(inputs) 
 highest = max(inputs)
     
 cap = int(math.sqrt(highest))
-# This would cause Memory error
+# This would cause Memory error, so we will use sqrt, and rely on the reduce function
 #cap = highest/2
 
+#----------------------------------------------------
 # Create a dictionary of primes
 # Basically the sieve of eratosthenes
 for j in range(2, cap ):
@@ -46,18 +53,33 @@ for j in range(2, cap ):
         
         for elem in inputs:
             if isFactor(elem, j):
-                print "found %i for %s" % (j, elem)
+                #print "found %i for %s" % (j, elem)
                 lpfs[elem].append(j)
                               
         for k in range (j+j, cap, j):
             #print 'eliminating %i' % k
             primes[k] = 0
+#----------------------------------------------------           
+ 
     
-#pprint.pprint(primes)
+if debug:
+    pprint.pprint(primes)
 
 for item in inputs:
+    max = 0
     try:
-        print "%i larges prime factor was %s" % (item, lpfs[item])
+        if debug:
+            print "%i larges prime factor was %s" % (item, lpfs[item])
+        mutable_value = item
+        for factor in lpfs[item]:
+            max = factor
+            mutable_value = reduce(mutable_value, factor)
+        if debug:
+            print "%i was max, last mutable ws %s" % (max, mutable_value)   
+        if mutable_value != 1:
+            print mutable_value
+        else:
+            print max
     except:
         print "%i is its own largest factor" % item
        
